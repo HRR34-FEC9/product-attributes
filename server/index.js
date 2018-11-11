b4a7edc69e176b31f30841885b868019d31a8910
JSEA <julius.sea@mac.com>
2018-11-10 20:30:52 -0500

    ready to deploy

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const models = require('./models/index');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
